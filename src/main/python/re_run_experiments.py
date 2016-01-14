fbb7a34d9353d60812459eac1c5d8aa1d46f23d1
Naimdjon Takhirov <naimdjon@gmail.com>
2016-01-14 23:26:24 +0100

    Rerun experiments for each file, removed logging.

--- src/main/python/re_run_experiments.py
+++ src/main/python/re_run_experiments.py
@@ -8,14 +8,13 @@ def main(argv):
     vfrbr_frbrize_marc = "/data/vfrbr/vfrbr-frbrize-marc"
     vfrbr_export = "/data/vfrbr/vfrbr-export"
     sql = os.path.join(vfrbr_persist, "src/main/resources/sql/ddl")
-    print sql
     if argv.__len__() != 1:
         print "Usage rerun_experiments <mrc_files>"
         sys.exit(1)
     for path, subdirs, files in os.walk(argv[0]):
         for ffile in files:
             print "processing ", os.path.abspath(path), ",", ffile
-            print "recreating DB schema..."
+            print "recreating DB schema..., path is", os.path.abspath(path)
             call(["bash", "script-recreate.sql"], cwd=sql)
             call(["bash", "batch-load.sh", "-Dmarc_data_path=" + os.path.abspath(path)], cwd=vfrbr_frbrize_marc)
             call(["bash", "rdf-export.sh", "-Dexport.path=" + os.path.abspath(path)], cwd=vfrbr_export)
